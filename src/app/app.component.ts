dc8531c857aafaff23a995963fa32fcd137314ea
jeroperales <jeronimoperales1@gmail.com>
2024-11-28 14:37:09 -0300

    added movement to background and got rid of margins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,23 @@ import { HomeComponent } from './components/home/home.component';
 export class AppComponent {
   title = 'jeroPortfolio';
 
+  onMouseMove(event: MouseEvent): void {
+    const container = event.currentTarget as HTMLElement;
+    const rect = container.getBoundingClientRect();
+
+    // Calcula la posición del mouse relativa al contenedor
+    const x = ((event.clientX - rect.left) / rect.width - 0.5) * 15; // Ajusta el valor para mayor o menor desplazamiento
+    const y = ((event.clientY - rect.top) / rect.height - 0.5) * 15;
+
+    // Actualiza la posición del fondo
+    container.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
+  }
+
+  resetBackground(): void {
+    // Restaura el fondo a la posición central
+    const container = document.querySelector('.background-container') as HTMLElement;
+    if (container) {
+      container.style.backgroundPosition = '50% 50%';
+    }
+  }
 }
